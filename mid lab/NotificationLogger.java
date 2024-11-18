be6d38fee50eaad4ad83d784fda51604b8b3a901
Ghaffar_Codes <135798061+Ghaffar0610@users.noreply.github.com>
2024-11-18 19:26:57 +0500

    Delete mid lab directory

--- mid lab/NotificationLogger.java
+++
@@ -1,7 +0,0 @@
-
-public class NotificationLogger implements TransportObserver {
-    @Override
-    public void update(String message) {
-        System.out.println("[NotificationLogger] Alert: " + message);
-    }
-}
--- mid lab/TransportObserver.java
+++
@@ -1,5 +0,0 @@
-
-public interface TransportObserver {
-    void update(String message);
-}
-
